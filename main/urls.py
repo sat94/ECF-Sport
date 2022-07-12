45a4e03bcc36a8493ca75c2e3e2355a36ac0a201
sat94 <sultan77@live.fr>
2022-07-12 22:42:57 +0200

    corection des url

--- main/urls.py
+++ main/urls.py
@@ -2,11 +2,14 @@ from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
 from .views import index
+from accounts.views import login
+from signup.views import signup
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index, name='index'), 
-    path('login', include("accounts.urls"), name="login"),
-    path('signup', include("signup.urls"), name="signup"),      
+    path("login/", login, name="login"),
+    path("signup/", signup, name="signup"),      
 ]
